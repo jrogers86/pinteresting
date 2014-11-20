082112c688b865f62d884c673d5d5d430dd4248c
Jonathan Rogers <jonathan.rogers86@gmail.com>
2014-11-20 10:36:47 +0000

    Update show view, add glyphicons and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about" #creates about_path
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
